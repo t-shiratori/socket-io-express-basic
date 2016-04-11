db67cac18ef40a40bf7ac9e66ad4e260c5b78308
白鳥貴士 <shiratori@waku-2.com>
2016-04-11 18:33:42 +0900

    修正

--- index.js
+++ index.js
@@ -9,27 +9,33 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// console.log('app: ',app);
+// console.log('http: ',http);
+// console.log('io: ',io);
+
 //ルームネーム
 var room1 = 'room0001';
 
 //ルーティング
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendfile('index.html');
 });
-
 
 /*--
 
   socket io
 
 -----------------------------------------------------*/
+
 //io()でもio.sockets()でも同じ
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
+
+  console.log(socket);
 
   //接続されたクライアントのソケットをルームに追加する
   socket.join(room1);
 
-  //console.log('-- connection --');
+  console.log('-- connected --');
 
   //console.log('socket: ',socket);
   //console.log('socket adapter: ',socket.adapter);
@@ -38,7 +44,7 @@ io.on('connection', function(socket){
   //console.log('id: ', socket.id);
 
   //送信者のみに送信
-　socket.emit('onlySendClient','your connected!');
+  socket.emit('onlySendClient', 'your connected!');
 
   //送信者以外にイベント送信
   socket.broadcast.emit('chat message', 'others start');
@@ -48,21 +54,19 @@ io.on('connection', function(socket){
   var res = {
     name: room1,
     data: socket.adapter.rooms[room1]
-  }
-  io.to(room1).emit('getRoomData',res);
+  };
+  io.to(room1).emit('getRoomData', res);
 
   //イベント設定
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function (msg) {
     //console.log('msg: ',msg);
-    io.emit('chat message', msg);
+    io.to(room1).emit('chat message', msg);
   });
 
 });
 
-
-
 //herokuはポート番号を動的に生成する。
 //本番環境における動的なポート番号の生成をサポートするために、以下のように書かく。
-http.listen(process.env.PORT || 5000, function(){
+http.listen(process.env.PORT || 5000, function () {
   //console.log('Node app is running on port', app.get('port'));
 });
